756dba9ddba76ed4cd2bc5147d76906d2d18f19d
Erwin Andino <erwin.andino@hotmail.com>
2025-06-11 16:03:36 -0300

    se avanzo en el sistema de contraataque

--- scenes/game.js
+++ scenes/game.js
@@ -33,11 +33,14 @@ this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
 this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
+this.cooldown = 0
+
 //delay entre ataques del enemigo
-this.delay = 3000; 
+this.delay = Phaser.Math.Between(1000, 2000); 
+console.log(this.delay)
 
 // variable para decidir de quien es el turno de atacar
-this.attackTurn = "enemy";
+this.attackTurn = 1;
 
 //crear un temporizador para el ataque del enemigo
 this.attackTimer = this.time.addEvent({
@@ -54,6 +57,13 @@ this.left = this.add.image(660, 540, "star").setScale(4).setAlpha(0.5).setTint(0
 this.right = this.add.image(1260, 540, "star").setScale(4).setAlpha(0.5).setTint(0xff2a00).setVisible(false);
 this.down = this.add.image(960, 840, "star").setScale(4).setAlpha(0.5).setTint(0x51ff00).setVisible(false);
 
+
+// this.attack2 = {
+// UP: this.up,
+// LEFT : this.left,
+// RIGHT : this.right,
+// DOWN : this.down,
+// }
 // vida del jugador
 this.healthPlayer = 100;
 
@@ -84,28 +94,70 @@ this.stunText = this.add.text(960, 32, `Stun: ${this.stun}`, {
   }
   update() {
 //Detectar teclas
+if(this.cooldown != 1) {
      if (Phaser.Input.Keyboard.JustDown(this.keyW) || (Phaser.Input.Keyboard.JustDown(this.cursors.up))) {
 console.log("W or Up Arrow pressed");
         this.playerAction = 1; // Set player action to 1 for upward attack
+        if (this.attackTurn === 2) {
+        this.up.setVisible(true)
+        this.left.setVisible(false)
+        this.right.setVisible(false)
+        this.down.setVisible(false)
+         this.playerAttack();
+        }
      }
      if (Phaser.Input.Keyboard.JustDown(this.keyA) || (Phaser.Input.Keyboard.JustDown(this.cursors.left))) {
 console.log("A or Left Arrow pressed");
         this.playerAction = 2; // Set player action to 2 for leftward attack
+              if(this.attackTurn === 2) {
+        this.up.setVisible(false)
+        this.left.setVisible(true)
+        this.right.setVisible(false)
+        this.down.setVisible(false)
+         this.playerAttack();
+        }
      }
      if (Phaser.Input.Keyboard.JustDown(this.keyS) || (Phaser.Input.Keyboard.JustDown(this.cursors.down))) {
 console.log("S or Down Arrow pressed");
         this.playerAction = 3; // Set player action to 3 for downward attack
+              if(this.attackTurn === 2) {
+        this.up.setVisible(false)
+        this.left.setVisible(false)
+        this.right.setVisible(false)
+        this.down.setVisible(true)
+         this.playerAttack();
+        }
      }
      if (Phaser.Input.Keyboard.JustDown(this.keyD) || (Phaser.Input.Keyboard.JustDown(this.cursors.right))) {
 console.log("D or Right Arrow pressed");
         this.playerAction = 4; // Set player action to 4 for rightward attack
+              if(this.attackTurn === 2) {
+        this.up.setVisible(false)
+        this.left.setVisible(false)
+        this.right.setVisible(true)
+        this.down.setVisible(false)
+         this.playerAttack();
+        }
      }
+    }
+    
 
      //condicion de derrota al llegar la vida del jugador a 0
      if (this.healthPlayer <= 0) {
-       console.log("Game Over! Player has no health left.");
         this.attackTimer.paused = true; // Stop the enemy attack timer
-        this.add.text(960, 440, "Game Over", {
+        this.add.text(960, 440, "Game Over, you lose", {
+         fontSize: "64px",
+         fill: "#ff0000",
+       }).setOrigin(0.5, 0.5); // Center the text
+       this.time.delayedCall(3000, () => {
+         this.scene.restart(); // Restart the scene if player health is zero
+       });
+
+     }
+
+     if (this.healthEnemy <= 0) {
+        this.attackTimer.paused = true; // Stop the enemy attack timer
+        this.add.text(960, 440, "Game Over, you win", {
          fontSize: "64px",
          fill: "#ff0000",
        }).setOrigin(0.5, 0.5); // Center the text
@@ -116,46 +168,57 @@ console.log("D or Right Arrow pressed");
      }
 
      //stun del enemigo
-     if (this.stun === 100) {
+     if (this.stun >= 100) {
          console.log("Enemy is stunned!");
-         this.attackTurn = "player"; // Change turn to player
+         this.attackTurn = 2; // Change turn to player
          this.stun = 0; // Reset stun after it reaches 100
 
-        //  this.time.delayedCall(5000, () => {
-        //     
-        //     this.stunText.setText(`Stun: ${this.stun}`); // Update stun display
-        //     this.attackTurn = "enemy"; // Change turn back to enemy
-        //  });
+         this.time.delayedCall(5000, () => {
+            
+            this.stunText.setText(`Stun: ${this.stun}`); // Update stun display
+            this.attackTurn = 1; // Change turn back to enemy
+            this.up.setVisible(false)
+        this.left.setVisible(false)
+        this.right.setVisible(false)
+        this.down.setVisible(false)
+         this.playerAction = 0;
+         });
      }
   }
 //empiezan las funciones
 turnDecider() {
-    if (this.attackTurn === "enemy") {
+  console.log(this.attackTurn)
+    if (this.attackTurn === 1) {
       this.enemyAttack(); // Call enemy attack function
-    
+    console.log("enemy")
     } 
-    if (this.attackTurn === "player") {
-      this.playerAttack(); // Call player attack function
-    }
+    // if (this.attackTurn === 2) {
+    //   this.playerAttack(); // Call player attack function
+    //   console.log("player")
+    // }
   }
   enemyAttack() {
+    this.up.setVisible(false)
+        this.left.setVisible(false)
+        this.right.setVisible(false)
+        this.down.setVisible(false)
     let attackType = Phaser.Math.Between(1, 4);
 
     this.colorDecider(attackType); // Set the attack color based on the attack type
     this.attackColor.setVisible(true); // Show the attack direction
     this.time.addEvent({
-        delay: this.delay - 1000, // Delay before checking the player's defense
+        delay: this.delay - (this.delay/4), // Delay before checking the player's defense
       callback: () => {
         if (this.playerAction === attackType) {
           console.log("Player successfully defends against the enemy attack!");
-          this.enemy.setTint(0xff2a00); // Change enemy color to red on successful defense
+          this.enemy.setTint(0x00ff00); // Change enemy color to green on successful defense
           this.time.addEvent({
-            delay: 500, // Reset enemy color after a short delay
+            delay: 100, // Reset enemy color after a short delay
             callback: () => {
               this.enemy.clearTint();
             },
           });
-            this.stun += 20; // Increase stun by 20 on successful defense
+            this.stun +=  10; // Increase stun by 20 on successful defense
             this.stunText.setText(`Stun: ${this.stun}`); // Update stun display
         } else {
           console.log("Player fails to defend against the enemy attack!");
@@ -163,23 +226,17 @@ turnDecider() {
           this.healthPlayerText.setText(`Health: ${this.healthPlayer}`); // Update health display
         }
         this.playerAction = 0; // Reset player action after checking
-        this.attackColor.setVisible(false); // Hide the attack direction
+        
+        this.delay = Phaser.Math.Between(500, 1000);
       },
 
     });
 
   }
   playerAttack() {
-    let attackType = this.playerAction; // Get the player's attack type
-    this.colorDecider(attackType); // Set the attack color based on the attack type
-     if (this.attackColor === null) {
-        console.log("No valid attack color, aborting playerAttack.");
-        return; // Sale de la función si no hay color válido
+    if (this.playerAction === 0) { //si no se presiono nada se cancela el ataque
+    return
     }
-    this.attackColor.setVisible(true); // Show the attack direction
-    this.time.addEvent({
-        delay: this.delay - 1000, // Delay before checking the player's defense
-      callback: () => {
         this.enemy.setTint(0xff2a00); // Change enemy color to red on successful defense
         this.time.addEvent({
             delay: 500, // Reset enemy color after a short delay
@@ -188,12 +245,19 @@ turnDecider() {
             },
           });
           this.playerAction = 0; // Reset player action after checking
-        this.attackColor.setVisible(false); // Hide the attack direction
+  
         this.healthEnemy -= 20; // Reduce enemy health by 20 on successful attack
+        this.healthEnemyText.setText(`Enemy: ${this.healthEnemy}`);
+        this.cooldown = 1;
+        this.time.delayedCall(1000, () => {
+         this.cooldown = 0;
+        this.up.setVisible(false);
+        this.left.setVisible(false);
+        this.right.setVisible(false);
+        this.down.setVisible(false);
+       });
       }
-      
-    });
-  }
+  
   colorDecider(attackType) {
    switch (attackType) {
       case 1:
@@ -213,4 +277,5 @@ turnDecider() {
         console.log("Unknown attack type");
     } 
   }
+
 }
